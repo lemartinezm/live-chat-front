20a440953a722705b63526daa876e8c1bf47c012
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-16 17:18:07 -0500

    feat: room page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import "./App.css";
-
-const socket = io(import.meta.env.VITE_API_URL);
+import Room from "./pages/Room";
 
 function App() {
-  const [currentMessage, setCurrentMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    socket.on("chat message", (msg) => {
-      setMessages((prev) => [...prev, msg]);
-    });
-
-    return () => {
-      socket.off("chat message");
-    };
-  }, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    socket.emit("chat message", currentMessage);
-    setCurrentMessage("");
-  };
-
   return (
-    <main className="App">
-      <ul id="messages">
-        {messages.map((message, index) => (
-          <li key={`msg-${index}`}>{message}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="input"
-          name="input"
-          placeholder="Write your message here"
-          value={currentMessage}
-          onChange={(e) => setCurrentMessage(e.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
-    </main>
+    <>
+      <Room />
+    </>
   );
 }
 
---
+++ src/pages/Room.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io(import.meta.env.VITE_API_URL);
+
+function Room() {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("chat message", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+
+    return () => {
+      socket.off("chat message");
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("chat message", currentMessage);
+    setCurrentMessage("");
+  };
+  return (
+    <main className="App">
+      <ul id="messages">
+        {messages.map((message, index) => (
+          <li key={`msg-${index}`}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="input"
+          name="input"
+          placeholder="Write your message here"
+          value={currentMessage}
+          onChange={(e) => setCurrentMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </main>
+  );
+}
+
+export default Room;
